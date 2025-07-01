609bfd91d7500b086dcdaa8170dc369f80015705
Felipe Gomes de Melo <felipegmelo.42@gmail.com>
2025-07-01 09:49:32 -0300

    Read position from yellow robot

--- controllers/referee_controller/referee_controller.cpp
+++ controllers/referee_controller/referee_controller.cpp
@@ -14,15 +14,9 @@
 #include <array>
 #include <span>
 #include <iostream>
-#include <iomanip> // for std::fixed and std::setprecision
+#include <unordered_map>
+#include <iomanip>
 
-// This is the main program of your controller.
-// It creates an instance of your Robot instance, launches its
-// function(s) and destroys it at the end of the execution.
-// Note that only one instance of Robot should be created in
-// a controller program.
-// The arguments of the main function can be specified by the
-// "controllerArgs" field of the Robot node
 int main(int argc, char **argv)
 {
 
@@ -37,18 +31,10 @@ int main(int argc, char **argv)
 
   std::cout << "Get robot" << "\n";
 
-  // webots::Node *yellow_robot = referee->getFromDef("YellowRobot0");
-
   webots::Node *root = referee->getRoot();
-  std::cout << "Passou da chamada\n" << "Node type: " << root->getType() << "\n";
-  // if (!yellow_robot)
-  // {
-  //   std::cout << "Quebrou!\n";
-  //   // std::exit(-1);
-  // }
 
   std::cout << "Get children\n";
-  webots::Field* children = root->getField("children");
+  webots::Field *children = root->getField("children");
 
   if (!children)
   {
@@ -59,38 +45,36 @@ int main(int argc, char **argv)
   auto count = children->getCount();
   std::cout << "Node count: " << count << std::endl;
 
+  std::unordered_map<std::string, webots::Node *> entities = {
+      {"YellowRobot0", nullptr},
+      {"YellowRobot1", nullptr},
+      {"YellowRobot2", nullptr},
+      {"BlueRobot0", nullptr},
+      {"BlueRobot1", nullptr},
+      {"BlueRobot2", nullptr},
+  };
+
   for (int i = 0; i < count; i++)
   {
-    // std::cout << "node\n";
-    webots::Node * node =  children->getMFNode(i);
+    webots::Node *node = children->getMFNode(i);
     auto *name = node->getField("name");
-    if(name)
+    if (name)
+    {
       std::cout << "-> " << name->getSFString() << std::endl;
+      entities[name->getSFString()] = node;
+    }
     else
       std::cout << "-> " << "NoName" << std::endl;
   }
-                                                      
-  webots::Node *yellow_robot = referee->getFromDef("YellowRobot0");
+
+  auto yellow_robot = entities["YellowRobot0"];
+
   if (!yellow_robot)
   {
     std::cout << "Quebrou 3!\n";
     std::exit(-1);
   }
-  
-
-  // You should insert a getDevice-like function in order to get the
-  // instance of a device of the robot. Something like:
-  //  Motor *motor = robot->getMotor("motorname");
-  //  DistanceSensor *ds = robot->getDistanceSensor("dsname");
-  //  ds->enable(timeStep);
-
-  // Main loop:
-  // - perform simulation steps until Webots is stopping the controller
-  // std::cout << std::fixed << std::setprecision(2);
-
-  // const double (*position)[3] = reinterpret_cast<const double (*)[3]>(yellow_robot->getPosition());
-  // std::cout << position[0] << ", " << position[1] << ", " << position[2] << "\n";
-
+  std::cout << std::fixed << std::setprecision(2);
   while (referee->step(time_step) != -1)
   {
 
@@ -99,10 +83,10 @@ int main(int argc, char **argv)
     // std::span<const double, 3> position(root->getPosition(), 3);
     // std::cout << "Position: ";
 
-
     // const double (*position)[3] = reinterpret_cast<const double (*)[3]>(yellow_robot->getPosition());
+    const double *position = yellow_robot->getPosition();
 
-    // std::cout << position[0] << ", " << position[1] << ", " << position[2] << "\n";
+    std::cout << position[0] << ", " << position[1] << ", " << position[2] << "\n";
   };
 
   // Enter here exit cleanup code.
