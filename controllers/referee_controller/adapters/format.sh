3b5c5573c974de8748ac7c8a08231fd7c955503b
Felipe Gomes de Melo <felipegmelo@usp.br>
2025-07-04 18:54:53 -0300

    Remove unused files from travesim adapters

--- controllers/referee_controller/adapters/format.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-HEADER_FILES=$(find . -name "*.hpp")
-SOURCE_FILES=$(find . -name "*.cpp")
-
-uncrustify -c uncrustify.cfg --replace --no-backup $HEADER_FILES
-
-uncrustify -c uncrustify.cfg --replace --no-backup $SOURCE_FILES
