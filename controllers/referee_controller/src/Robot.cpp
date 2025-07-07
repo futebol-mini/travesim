567a4f52fc489b1e22f77c15a68bfd8136519395
Felipe Gomes de Melo <felipegmelo.42@gmail.com>
2025-07-07 16:22:32 -0300

    Fix logic error in yaw calculation

--- controllers/referee_controller/src/Robot.cpp
+++ controllers/referee_controller/src/Robot.cpp
@@ -2,22 +2,18 @@
 #include <cmath>
 #include "Robot.hpp"
 
-std::array<double, 3> vss::Robot::get_position()
-{
-    const double *position = this->robot_ptr->getPosition();
+std::array<double, 3> travesim::webots_adapter::Robot::get_position() {
+    const double* position = this->robot_ptr->getPosition();
     return std::array<double, 3>{position[0], position[1], position[2]};
 }
 
-travesim::Vector2D vss::Robot::get_position2d()
-{
+travesim::Vector2D travesim::webots_adapter::Robot::get_position2d() {
     auto position = this->get_position();
     return travesim::Vector2D(position[0], position[1]);
 }
 
-double vss::Robot::get_yaw()
-{
-    const double * rotation_matrix = this->robot_ptr->getOrientation();
+double travesim::webots_adapter::Robot::get_yaw() {
+    const double* rotation_matrix = this->robot_ptr->getOrientation();
 
-    return atan2(rotation_matrix[0], rotation_matrix[3]);
-
+    return atan2(rotation_matrix[3], rotation_matrix[0]);
 }
