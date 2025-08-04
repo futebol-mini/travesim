b91de5ba28605f9b175e1b14046938610b6c8302
Felipe Gomes de Melo <felipegmelo@usp.br>
2025-08-04 18:30:53 -0300

    Log robots positions in each frame

--- controllers/camera_controller/camera_controller.py
+++ controllers/camera_controller/camera_controller.py
@@ -2,56 +2,82 @@
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
+import os
+import sys
+from datetime import datetime
+from math import atan2
+from typing import cast
+
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 from controller import Camera, Supervisor
 
-# create the Robot instance.
-robot = Supervisor()
+ROBOTS = [
+    "YellowRobot0",
+    "YellowRobot1",
+    "YellowRobot2",
+    "BlueRobot0",
+    "BlueRobot1",
+    "BlueRobot2",
+    "VssBall",
+]
 
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
+supervisor = Supervisor()
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)
+children = supervisor.getRoot().getField("children")
 
-camera = robot.getDevice("camera")
+robot_dict = {}
+
+for i in range(children.count):
+    try:
+        scene_node = children.getMFNode(i)  # type: ignore
+        robot_dict[scene_node.getField("name").getSFString()] = scene_node
+    except:  # noqa: E722
+        pass
+
+timestep = int(supervisor.getBasicTimeStep())
+
+camera = cast(Camera, supervisor.getDevice("camera"))
 camera.enable(timestep)
+
+uuid = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+
+os.makedirs(f"output/{uuid}")
 
 # Get camera dimensions
 width = camera.getWidth()
 height = camera.getHeight()
 
-# Setup matplotlib for live updating
-plt.ion()  # Turn on interactive mode
-fig, ax = plt.subplots()
-ax.set_title("Webots Camera Feed")
-image_plot = ax.imshow(np.zeros((height, width, 3)))
-plt.tight_layout()
-plt.show(block=False)
+print("Camera started")
 
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
-    img = camera.getImage()
-    if img:
-        # Convert image to numpy array
-        np_image = np.frombuffer(img, dtype=np.uint8)
-        np_image = np_image.reshape((height, width, 4))
+frame = 0
+with open(f"output/{uuid}/data.csv", "w") as output_file:
+    output_file.write("image")
+    for robot in ROBOTS:
+        if robot == "VssBall":
+            output_file.write(f",{robot}_x,{robot}_y")
+        else:
+            output_file.write(f",{robot}_x,{robot}_y,{robot}_yaw")
+    output_file.write("\n")
 
-        # Update plot data
-        image_plot.set_data(cv2.cvtColor(np_image, cv2.COLOR_BGRA2RGB))
+    while supervisor.step(timestep) != -1 and frame < 1000:  # type: ignore
+        frame += 1
+        image_name = f"output/{uuid}/{uuid}_{frame:04d}.png"
+        camera.saveImage(image_name, 100)
 
-        # Refresh matplotlib display
-        fig.canvas.flush_events()
-    else:
-        print("Warning: No image data received")
+        output_file.write(image_name)
+        for robot in ROBOTS:
+            pos = robot_dict[robot].getPosition()
+            rot = robot_dict[robot].getOrientation()
+            if robot == "VssBall":
+                output_file.write(f",{pos[0]:.5f},{pos[1]:.5f}")
+            else:
+                yaw = atan2(rot[3], rot[0])
+                output_file.write(f",{pos[0]:.5f},{pos[1]:.5f},{yaw:.5f}")
 
-# Enter here exit cleanup code.
-plt.ioff()
-plt.close()
+        output_file.write("\n")
+        print(f"Frame: {frame}")
+
+supervisor.simulationSetMode(Supervisor.SIMULATION_MODE_PAUSE)  # type: ignore
 print("Controller terminated")
